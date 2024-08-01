2bba8a9279d69e2a30c98eaffce7ac3d60560d5e
o.stoliarchuk <thesanimoneyglobal@gmail.com>
2024-08-01 19:46:20 +0300

    added server side ml

--- schema/index.ts
+++ schema/index.ts
@@ -3,13 +3,13 @@ import * as z from "zod";
 export const formSchema = z.object({
     numberOfItems: z.string()
         .max(4, { message: "numberOfItems must be at most 4 characters long" })
-        .refine((val) => /^[0-9]+$/.test(val), {
-            message: "numberOfItems must be a number only",
+        .refine((val) => /^[1-9][0-9]*$|^0$/.test(val), {
+            message: "numberOfItems must be a valid number without leading zeros",
         }),
     numberOfScores: z.string()
         .max(4, { message: "numberOfScores must be at most 4 characters long" })
-        .refine((val) => /^[0-9]+$/.test(val), {
-            message: "numberOfScores must be a number only",
+        .refine((val) => /^[1-9][0-9]*$|^0$/.test(val), {
+            message: "numberOfScores must be a valid number without leading zeros",
         }),
     formType: z.string(),
     branchingLogic: z.boolean(),
